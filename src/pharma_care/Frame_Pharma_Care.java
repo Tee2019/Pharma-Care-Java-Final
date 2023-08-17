4492c2386181650e7ce240203f05c6e274c2dfe7
Tee2019 <tamanna.khatun.cse@ulab.edu.bd>
2023-08-17 13:38:36 +0600

    thiii

--- src/pharma_care/Frame_Pharma_Care.java
+++ src/pharma_care/Frame_Pharma_Care.java
@@ -7,7 +7,7 @@ public class Frame_Pharma_Care extends javax.swing.JFrame {
         
         initComponents();
     }
-
+//thi com
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
